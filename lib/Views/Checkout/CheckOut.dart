82d2b71b98bcae47d210fc9d7762ac7b72e23bcc
AhmadhassanTK <ahmadhassantk@gmail.com>
2024-03-11 01:44:02 +0200

    53-1

--- lib/Views/Checkout/CheckOut.dart
+++ lib/Views/Checkout/CheckOut.dart
@@ -1,23 +1,17 @@
-// ignore_for_file: prefer_const_literals_to_create_immutables, prefer_const_constructors
-
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:shoes_app/Models/Controller/CartController.dart';
 import 'package:shoes_app/Models/Controller/OrderController.dart';
-import 'package:shoes_app/Views/Checkout/Controller/CheckoutController.dart';
 import 'package:shoes_app/Views/Checkout/Widgets/AddressSection.dart';
 import 'package:shoes_app/Views/Checkout/Widgets/AmountSection.dart';
 import 'package:shoes_app/Views/Checkout/Widgets/PaymentSection.dart';
 import 'package:shoes_app/Views/Checkout/Widgets/checkoutItems.dart';
-import 'package:shoes_app/Views/NavigationMenu/Screens/NavigationMenu.dart';
 import 'package:shoes_app/utils/constants/colors.dart';
-import 'package:shoes_app/utils/constants/image_strings.dart';
 import 'package:shoes_app/utils/constants/sizes.dart';
 import 'package:shoes_app/utils/helpers/pricing_calculator.dart';
 import 'package:shoes_app/utils/shared/CAppBar.dart';
 import 'package:shoes_app/utils/shared/CCouponCode.dart';
 import 'package:shoes_app/utils/shared/CRoundedContainer.dart';
-import 'package:shoes_app/utils/shared/CSuccessScreen.dart';
 
 class CheckoutView extends StatelessWidget {
   const CheckoutView({super.key});
@@ -43,27 +37,27 @@ class CheckoutView extends StatelessWidget {
         ),
         showbackarrow: true,
       ),
-      body: SingleChildScrollView(
+      body: const SingleChildScrollView(
         child: Padding(
-          padding: const EdgeInsets.all(CSizes.defaultSpace),
+          padding: EdgeInsets.all(CSizes.defaultSpace),
           child: Column(
             children: [
-              const CheckoutItems(),
-              const SizedBox(height: CSizes.spaceBtwSections),
-              const CCouponCode(),
-              const SizedBox(height: CSizes.spaceBtwSections),
+              CheckoutItems(),
+              SizedBox(height: CSizes.spaceBtwSections),
+              CCouponCode(),
+              SizedBox(height: CSizes.spaceBtwSections),
               CRoundedContainer(
                 showborder: true,
                 bordercolor: CColors.grey,
-                padding: const EdgeInsets.all(CSizes.md),
+                padding: EdgeInsets.all(CSizes.md),
                 child: Column(
                   children: [
                     AmountSection(),
-                    const SizedBox(height: CSizes.spaceBtwItems / 2),
+                    SizedBox(height: CSizes.spaceBtwItems / 2),
                     Divider(),
-                    const SizedBox(height: CSizes.spaceBtwItems / 2),
+                    SizedBox(height: CSizes.spaceBtwItems / 2),
                     PaymentSection(),
-                    const SizedBox(height: CSizes.spaceBtwItems / 2),
+                    SizedBox(height: CSizes.spaceBtwItems / 2),
                     AddressSection(),
                   ],
                 ),
